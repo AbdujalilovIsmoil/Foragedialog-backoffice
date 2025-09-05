f4ed8bf66b8e54086d15fbed8a6abc384c3a5329
AbdujalilovIsmoil <ismoilbek20050421@gmail.com>
2025-09-06 03:19:46 +0500

    feat: Added NotFound page

--- src/pages/Not-Found/Not-Found.tsx
+++ src/pages/Not-Found/Not-Found.tsx
@@ -1,9 +1,31 @@
-function NotFound() {
+import { Button, Result } from "antd";
+import { NotFoundSection } from "./style";
+import { useNavigate } from "react-router-dom";
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>NotFound</h1>
-    </div>
+    <NotFoundSection>
+      <Result
+        title="404"
+        status="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button
+            type="primary"
+            onClick={() =>
+              navigate({
+                pathname: "/",
+              })
+            }
+          >
+            Back Home
+          </Button>
+        }
+      />
+    </NotFoundSection>
   );
-}
+};
 
 export default NotFound;
---
+++ src/pages/Not-Found/style.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import styled from "styled-components";
+
+export const NotFoundSection = styled.section`
+  width: 100%;
+  display: flex;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
